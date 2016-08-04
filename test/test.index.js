1be85549aca67a01f81c57f9cc00241f476229e1
#云淡然 <cloudcome@163.com>
2016-08-04 15:12:46 +0800

    + blear.classes.count-down

--- test/test.index.js
+++ test/test.index.js
@@ -10,7 +10,7 @@
 var CountDown = require('../src/index.js');
 
 describe('测试文件', function () {
-    it('base', function (done) {
+    it('参数为数字', function (done) {
         var cd = new CountDown(10000);
 
         cd.on('start', function () {
@@ -18,7 +18,27 @@ describe('测试文件', function () {
         });
 
         cd.on('change', function (remain) {
-            console.log('change', Math.ceil(remain/1000));
+            console.log('change', Math.ceil(remain / 1000));
+        });
+
+        cd.on('stop', function () {
+            console.log('stop');
+            expect(cd.is()).toEqual(false);
+            done();
+        });
+
+        cd.start();
+    }, 20000);
+
+    it('参数为对象', function (done) {
+        var cd = new CountDown({count: 4000});
+
+        cd.on('start', function () {
+            console.log('start');
+        });
+
+        cd.on('change', function (remain) {
+            console.log('change', Math.ceil(remain / 1000));
         });
 
         cd.on('stop', function () {
