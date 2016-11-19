69b10397e94c5a8191a4a50b31feccdeb260fc70
#云淡然 <cloudcome@163.com>
2016-11-19 11:53:53 +0800

    ~ 更加丰富和健壮的接口

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
- * blear.classes.count-down
+ * blear.classes.countdown
  * @author ydr.me
- * @create 2016年06月04日14:09:36
+ * @create 2016年11月19日11:50:03
  */
 
 'use strict';
@@ -9,21 +9,25 @@
 var time = require('blear.utils.time');
 var object = require('blear.utils.object');
 var typeis = require('blear.utils.typeis');
+var date = require('blear.utils.date');
 var Events = require('blear.classes.events');
 
-
+var STATE_READY = 0;
+var STATE_STARTED = 1;
+var STATE_PAUSED = 3;
+var STATE_DESTROYED = 5;
 var defaults = {
     /**
-     * 倒计时长度，单位：ms
+     * 时间间隔
      * @type Number
      */
-    count: 60000,
+    interval: 1000,
 
     /**
-     * 步长，单位：ms
+     * 时间总数，-1 为无限计时
      * @type Number
      */
-    interval: 1000
+    count: 60000
 };
 var CountDown = Events.extend({
     className: 'CountDown',
@@ -36,13 +40,20 @@ var CountDown = Events.extend({
             };
         }
 
-        the[_options] = object.assign({}, defaults, options);
+        options = the[_options] = object.assign({}, defaults, options);
+
+        if (typeis.Date(options.count)) {
+            options.count = options.count.getTime() - date.now();
+        }
+
+        the[_count] = options.count;
+        the.state = STATE_READY;
         CountDown.parent(the);
     },
 
 
     /**
-     * 计时器开始
+     * 开始计时
      * @returns {CountDown}
      */
     start: function () {
@@ -52,51 +63,157 @@ var CountDown = Events.extend({
             return the;
         }
 
-        var options = the[_options];
-
-        the.emit('start');
         var timer = the[_timer] = time.setInterval(function () {
-            var remain = options.count - timer.elapsedTime;
+            var elapsedTime = timer.elapsedTime;
+            var remainTime = 0;
 
-            if (remain <= 0) {
-                time.clearInterval(timer);
-                the[_timer] = null;
-                the.emit('change', 0);
-                the.emit('stop');
+            if (the[_options].count !== -1) {
+                remainTime = the[_count] - elapsedTime;
+            }
+
+            if (remainTime < 0) {
+                the.stop();
                 return;
             }
 
-            the.emit('change', remain);
-        }, options.interval, true);
-
+            the.emit('change', remainTime, elapsedTime);
+        }, the[_options].interval, true);
+        the.state = STATE_STARTED;
         return the;
     },
 
 
     /**
-     * 设置时间长度
+     * 获取已经流逝的时间
+     * @returns {Number}
+     */
+    getElapsedTime: function () {
+        var timer = this[_timer];
+
+        if (timer) {
+            return timer.elapsedTime;
+        }
+
+        return 0;
+    },
+
+
+    /**
+     * 获取剩余的时间
+     * @returns {Number}
+     */
+    getRemainTime: function () {
+        var the = this;
+
+        if (the[_options].count > 0) {
+            return the[_count] - the.getElapsedTime();
+        }
+
+        return 0;
+    },
+
+
+    /**
+     * 暂停计时
+     * @returns {CountDown}
+     */
+    pause: function () {
+        var the = this;
+
+        if (!the[_timer] || the.state === STATE_DESTROYED) {
+            return the;
+        }
+
+        time.clearInterval(the[_timer]);
+        the[_count] -= the[_timer].times * the[_options].interval;
+        the[_timer] = null;
+        the.state = STATE_PAUSED;
+        return the;
+    },
+
+
+    /**
+     * 恢复计时
+     * @returns {*|CountDown}
+     */
+    resume: function () {
+        return this.start();
+    },
+
+
+    /**
+     * 停止计时器
+     * @returns {CountDown}
+     */
+    stop: function () {
+        var the = this;
+
+        if (!the[_timer]) {
+            return the;
+        }
+
+        the[_count] = 0;
+        time.clearInterval(the[_timer]);
+        the[_timer] = null;
+        the.state = STATE_READY;
+        time.nextTick(function () {
+            the.emit('change', 0, the[_count]);
+            the.emit('stop');
+        });
+        return the;
+    },
+
+
+    /**
+     * 设置总时间
      * @param count
      * @returns {CountDown}
      */
     setCount: function (count) {
         var the = this;
 
-        the[_options].count = count;
+        if (typeis.Date(count)) {
+            count = count.getTime() - date.now();
+        }
 
+        the[_count] = the[_options].count = count;
         return the;
     },
 
 
     /**
-     * 判断是否在倒计时
+     * 重置计时
+     * @returns {CountDown}
+     */
+    reset: function () {
+        return this.setCount(this[_options].count);
+    },
+
+
+    /**
+     * 判断是否正在计时
      * @returns {boolean}
      */
     is: function () {
-        return !!this[_timer];
+        return Boolean(this[_timer]);
+    },
+
+
+    /**
+     * 销毁实例
+     */
+    destroy: function () {
+        var the = this;
+
+        the.stop();
+        the.state = STATE_DESTROYED;
+        CountDown.parent.destroy(the);
     }
 });
 var _options = CountDown.sole();
 var _timer = CountDown.sole();
+var _count = CountDown.sole();
+
 
 CountDown.defaults = defaults;
 module.exports = CountDown;
--- test/test.index.js
+++ test/test.index.js
@@ -49,4 +49,122 @@ describe('测试文件', function () {
 
         cd.start();
     }, 20000);
+
+    it('#start', function (done) {
+        var cd = new CountDown({
+            count: 100,
+            interval: 10
+        });
+        var times = 0;
+
+        cd.start();
+        cd.start();
+        cd.on('change', function () {
+            times++;
+        });
+        cd.on('stop', function () {
+            expect(times).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    it('#pause/resume', function (done) {
+        var cd = new CountDown({
+            count: 200,
+            interval: 10
+        });
+        var times = 0;
+
+        cd.start();
+        cd.start();
+        cd.on('change', function () {
+            times++;
+
+            if (times === 2) {
+                cd.pause();
+                cd.pause();
+                cd.resume();
+            }
+        });
+        cd.on('stop', function () {
+            expect(times).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    it('#setCount', function (done) {
+        var cd = new CountDown({
+            count: 100,
+            interval: 10
+        });
+        var times = 0;
+
+        cd.start();
+        cd.setCount(200);
+        cd.on('change', function () {
+            times++;
+        });
+        cd.on('stop', function () {
+            expect(times).toBeGreaterThan(1);
+            done();
+        });
+    });
+
+    it('#reset', function (done) {
+        var cd = new CountDown({
+            count: 100,
+            interval: 10
+        });
+        var times = 0;
+        var reset = false;
+
+        cd.start();
+        cd.on('change', function () {
+            times++;
+        });
+        cd.on('stop', function () {
+            if (reset) {
+                expect(times).toBeGreaterThan(2);
+                done();
+                return;
+            }
+
+            reset = true;
+            expect(times).toBeGreaterThan(1);
+            cd.reset();
+            cd.start();
+        });
+    });
+
+    it('#stop、#getElapsedTime、#getRemainTime', function (done) {
+        var cd = new CountDown({
+            count: 200,
+            interval: 10
+        });
+
+        cd.stop();
+        cd.start();
+        cd.on('change', function (remainTime, elapsedTime) {
+            expect(cd.getRemainTime()).toBe(remainTime);
+            expect(cd.getElapsedTime()).toBe(elapsedTime);
+        });
+        cd.stop();
+        cd.on('stop', function () {
+            expect(cd.getRemainTime()).toBe(0);
+            done();
+        });
+    });
+
+    it('#destroy', function (done) {
+        var timer = new CountDown({
+            count: 200,
+            interval: 10
+        });
+
+        timer.start();
+        timer.destroy();
+        timer.on('stop', function () {
+            done();
+        });
+    });
 });
